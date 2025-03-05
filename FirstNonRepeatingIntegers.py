960bedb2f3ca19406b37306cbbae94a862c90226
Muhammad Usman <ranasaani@gmail.com>
2025-03-05 14:46:24 +0000

    9,564,771

--- FirstNonRepeatingIntegers.py
+++ FirstNonRepeatingIntegers.py
@@ -1,11 +1,9 @@
-def non_repeating(arr):
-    non_repeating = []
-    for n in arr:
-        if n in non_repeating:
-            non_repeating.pop(non_repeating.index(n))
-        else:
-            non_repeating.append(n)
-
-    return non_repeating[0] if non_repeating else None
-
-print(non_repeating([1, 1, 1, 5, 2, 1, 3, 4, 2]))
+def find_first_non_repeating(arr):
+    counts = {}
+    for x in arr:
+        counts[x] = counts.get(x, 0) + 1
+    
+    for x in arr:
+        if counts[x] == 1:
+            return x
+    return None
